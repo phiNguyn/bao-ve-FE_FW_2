54610dc762b3169f6488aa61f36735a609739fe4
Phi Nguyen <phinguyenq12@gmail.com>
2024-08-10 16:17:39 +0700

    delete some thing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import ProductList from './user/ProductList';
 import Header from './user/Header';
-import AdminProductList from './admin/adminProductList';
-import ProductAdd from './admin/ProductAdd';
 
 const App = () => {
   return (
@@ -12,8 +10,6 @@ const App = () => {
     
     <Route  path='/' element={<ProductList/>}  />
     <Route path='/products/:categoryId' element={<ProductList/>}  />
-    <Route path='/admin' element={<AdminProductList/>}/> 
-    <Route path='/admin/add' element={<ProductAdd/>}/> 
   </Routes>
     </>
 
